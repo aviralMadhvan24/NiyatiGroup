c47cf1609d6e8a27cbd6484e97ba6a73a5301b01
aviralMadhvan24 <aviralmadhvan@gmail.com>
2025-07-18 00:18:08 +0530

    logo added

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -95,9 +95,9 @@ const Header = () => {
                 className="bg-gray-900/50 border border-gray-800 w-10 h-10 md:w-12 md:h-12 rounded-lg flex items-center justify-center shadow-md"
                 whileHover={{ scale: 1.05 }}
               >
-                <span className="text-white font-bold text-xl md:text-2xl">
-                  N
-                </span>
+               
+                  <img src="/logo3.png" alt="" />
+              
               </motion.div>
               <motion.span
                 className="ml-3 text-white font-bold text-lg md:text-2xl"
