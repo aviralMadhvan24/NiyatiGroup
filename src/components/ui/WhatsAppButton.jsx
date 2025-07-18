c1107188d71076ed3333e9a28d50c446aede7ed1
aviralMadhvan24 <aviralmadhvan@gmail.com>
2025-07-18 16:57:34 +0530

    Update WhatsApp button link to the correct number

--- src/components/ui/WhatsAppButton.jsx
+++ src/components/ui/WhatsAppButton.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 const WhatsAppButton = () => {
   return (
     <motion.a
-      href="https://wa.me/919876543210"
+      href="https://wa.me/+919997070599"
       target="_blank"
       rel="noopener noreferrer"
       className=" fixed bottom-6 right-6 z-40"
